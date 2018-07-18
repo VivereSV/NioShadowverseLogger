cf58cc6cd6c8b356dd23f4112635c55a746a1942
VivereSV <32445070+VivereSV@users.noreply.github.com>
2018-07-18 02:17:55 -0500

    Update nio.py

--- nio.py
+++ nio.py
@@ -15,7 +15,8 @@ logfile = str(month) + "-" + str(day) + "-" + str(year) + "-log.txt"
 exepath = os.path.dirname(sys.executable)
 config = open(exepath + "/resources/config.txt", "r")
 folder = config.read()
-path = exepath + "/resources/" + folder + "/"
+lines = folder.split('\n')
+path = exepath + "/resources/" + lines[0] + "/"
 logpath = exepath + "/resources/" + logfile
 
 
@@ -44,6 +45,7 @@ img1 = Image.open(path + "1st.png")
 img2 = Image.open(path + "2nd.png")
 imgV = Image.open(path + "Victory.png")
 imgD = Image.open(path + "Defeat.png")
+miscs = [img1, img2, imgV, imgD]
 first = True
 win = True
 mullTaken = False
@@ -51,6 +53,22 @@ turnSet = False
 oppoSet = False
 resultSet = False
 yourSet = False
+c = 0.97
+
+if lines[1] == 'True':
+    print("Resizing images...")
+    icons = [icon.resize((icon.size[0] * 2, icon.size[1] * 2), Image.BICUBIC) for icon in icons]
+    posts = [post.resize((post.size[0] * 2, post.size[1] * 2), Image.BICUBIC) for post in posts]
+    miscs = [misc.resize((misc.size[0] * 2, misc.size[1] * 2), Image.BICUBIC) for misc in miscs]
+    c = 0.95
+    # for icon in icons:
+    #     icon = icon.resize((icon.size[0] * 2, icon.size[1] * 2), Image.BICUBIC)
+    # for post in posts:
+    #     post = post.resize((post.size[0] * 2, post.size[1] * 2), Image.BICUBIC)
+    # for misc in miscs:
+    #     misc = post.resize((post.size[0] * 2, post.size[1] * 2), Image.BICUBIC)
+    print("Images resized")
+
 
 print("Hi there! My name is Nio and I will be assisting you with automatically logging your matches! Fighting!" +
       "\nTo leave me alone and make me sad, just hit ctrl c on this window.")
@@ -58,15 +76,17 @@ try:
     while True:
         screen = pyautogui.screenshot()
         if not mullTaken:
-            if not pyautogui.locate(img1, screen, grayscale=True, confidence=0.9) is None:
+            if not pyautogui.locate(miscs[0], screen, grayscale=True, confidence=0.90) is None:
                 first = True
                 turnSet = True
-            elif not pyautogui.locate(img2, screen, grayscale=True, confidence=0.9) is None:
+                print("Wow you're going first!")
+            elif not pyautogui.locate(miscs[1], screen, grayscale=True, confidence=0.90) is None:
                 first = False
                 turnSet = True
+                print("Too bad you're going second...")
             if turnSet:
                 for icon in icons:
-                    if not pyautogui.locate(icon, screen, grayscale=True, confidence=0.9) is None:
+                    if not pyautogui.locate(icon, screen, grayscale=True, confidence=0.90) is None:
                         oppoClass = crafts[icons.index(icon)]
                         oppoSet = True
                         break
@@ -74,12 +94,14 @@ try:
             if mullTaken:
                 print("Okay, I've got your mulligan screen. Good luck!")
         else:
-            if not pyautogui.locate(imgD, screen, grayscale=False, confidence=0.9) is None:
+            if not pyautogui.locate(miscs[3], screen, grayscale=False, confidence=c) is None:
                 win = False
                 resultSet = True
-            elif not pyautogui.locate(imgV, screen, grayscale=False, confidence=0.9) is None:
+                print("Aww man, you'll get them next time!")
+            elif not pyautogui.locate(miscs[2], screen, grayscale=False, confidence=c) is None:
                 win = True
                 resultSet = True
+                print("You did it!")
             if resultSet:
                 for post in posts:
                     if not pyautogui.locate(post, screen, grayscale=True, confidence=0.9) is None:
